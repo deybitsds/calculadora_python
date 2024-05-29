caa2c7df0595cedf58a834be9469fa2e261f3630
David Prieto <deybitsds1@gmail.com>
2024-05-28 23:38:42 -0500

    interfaz 100%

--- modelo/model.py
+++ modelo/model.py
@@ -3,7 +3,12 @@ class Model:
     def __init__(self):
         self.input = "asd"
         self.output = "123"
- 
+        #self.undoredo = UndoRedo()
+
+    def evaluacion_postfija(self):
+        # self.input
+        pass
+
     def recuperar_input(self):
         return self.input
         
